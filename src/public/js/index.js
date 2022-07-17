974af9d4e44aaabc46865b09217d667845dc2075
mutuajames <njorogejames880@gmail.com>
2022-07-17 11:57:41 +0300

    Added a modal to the index page

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,24 @@
 'use strict';
+
+const openModalButtons = document.querySelectorAll('.open-modal'),
+	modal = document.querySelector('.modal'),
+	closeModalButtons = document.querySelectorAll('.close-modal');
+
+openModalButtons.forEach(openBtn => {
+	openBtn.addEventListener('click', openModal)
+});
+
+closeModalButtons.forEach(closeBtn => {
+	closeBtn.addEventListener('click', closeModal)
+});
+
+function openModal() {
+	modal.classList.add('visible');
+}
+
+function closeModal() {
+	modal.classList.remove('visible');
+}
 // get form  
 const file_sender = document.getElementById('file_sender');
 
@@ -8,164 +28,166 @@ const files_body = document.getElementById(`files_body`);
 // sockets
 const socket = io();
 
-socket.emit('data' , 'from front end');
+socket.emit('data', 'from front end');
 
 
 // random name generator
 
-  
-  
 
 
-file_sender?.addEventListener('submit' , async event =>{
-    event.preventDefault();
-    const { file } = event.target;
-    // text || file should be present before upload
-    console.log(file);
-    const form_data = new FormData();
-    form_data.set('file' , file.files[0]);
 
-    // reset form if everything goes well
-    await fetch('/files/' , {
-        method:'POST',
-        headers: new Headers({
-            // 'Content-Type':'application/json',
-        }),
-        body: form_data,
-    })
-    .then(data => data.json())
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error =>{
-        console.log(error);
-    });
-    event.target?.reset();
+
+file_sender?.addEventListener('submit', async event => {
+	event.preventDefault();
+	const { file } = event.target;
+	// text || file should be present before upload
+	console.log(file);
+	const form_data = new FormData();
+	form_data.set('file', file.files[0]);
+
+	// reset form if everything goes well
+	await fetch('/files/', {
+		method: 'POST',
+		headers: new Headers({
+			// 'Content-Type':'application/json',
+		}),
+		body: form_data,
+	})
+		.then(data => data.json())
+		.then(result => {
+			console.log(result)
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	event.target?.reset();
 });
 
 // get user files 
-const get_files = async () =>{
-    await fetch(`/files/`,
-    {
-        method:'GET',
-        headers: new Headers({
-            'Content-Type':'application/json',
-        }),
-        credentials:'same-origin',
-    })
-    .then(data => data.json())
-    .then(response =>{
-        console.log(response);
-        // document.getElementById(`files_body`).innerHTML = document.createElement(`li`).innerHTML = `No Files`;
-        if(response.status == 200){
-            response.files.forEach(( { download , name , locked , short_id } , index) =>{
-                li_creator({ 
-                    name,
-                    short_id,
-                    download,
-                    locked,
-                })
-            })
-        }else{
-            alert(response.message);
-        }
-    })
-    .catch(error =>{
-        console.log(error);
-    });
+const get_files = async () => {
+	await fetch(`/files/`,
+		{
+			method: 'GET',
+			headers: new Headers({
+				'Content-Type': 'application/json',
+			}),
+			credentials: 'same-origin',
+		})
+		.then(data => data.json())
+		.then(response => {
+			console.log(response);
+			// document.getElementById(`files_body`).innerHTML = document.createElement(`li`).innerHTML = `No Files`;
+			if (response.status == 200) {
+				response.files.forEach(({ download, name, locked, short_id }, index) => {
+					li_creator({
+						name,
+						short_id,
+						download,
+						locked,
+					})
+				})
+			} else {
+				alert(response.message);
+			}
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
 
-const network_service = async ({ path , method , body  }) =>{
-    await fetch(`${path}` , {
-        method,
-        headers: new Headers({
-            'Content-Type':'application/json',
-        }),
-        body: body ? JSON.stringify(body) : undefined,
-        credentials:'same-origin',
-    })
-    .then(data => data.json())
-    .then(response => {
-        if(response.status == 200){
-            alert(`Action taken`);
-        }else{
-            // someone kindly create a modal for displaying errors
-            alert(`${response.message}`);
-        }
-    })
+const network_service = async ({ path, method, body }) => {
+	await fetch(`${path}`, {
+		method,
+		headers: new Headers({
+			'Content-Type': 'application/json',
+		}),
+		body: body ? JSON.stringify(body) : undefined,
+		credentials: 'same-origin',
+	})
+		.then(data => data.json())
+		.then(response => {
+			if (response.status == 200) {
+				alert(`Action taken`);
+			} else {
+				// someone kindly create a modal for displaying errors
+				alert(`${response.message}`);
+			}
+		})
 }
 
 // download buffer file
-const download_file = async (id) =>{
-    await fetch(`/files/dowload/${id}`,{
-        method:`GET`,
-        headers: new Headers({
-            'Content-Type':'application/json',
-        }),
-        credentials:'same-origin',
-    })
-    .then((response) =>{
-        console.log(response);
-        alert(`File downloaded`);
-    })
-    .catch(error =>{
-        console.log(error);
-    })
+const download_file = async (id) => {
+	await fetch(`/files/dowload/${id}`, {
+		method: `GET`,
+		headers: new Headers({
+			'Content-Type': 'application/json',
+		}),
+		credentials: 'same-origin',
+	})
+		.then((response) => {
+			console.log(response);
+			alert(`File downloaded`);
+		})
+		.catch(error => {
+			console.log(error);
+		})
 }
 
 
 // li item creator 
-const li_creator = ({ name , short_id , download , locked }) =>{
-    // returns a li --- appended by the looper
-    // create parent
-    const li = document.createElement(`li`);
-    // 1st div
-    const div_one = document.createElement(`div`);
-    div_one.classList.add(`type`);
-    const i = document.createElement(`i`);
-    i.classList.add(`fa-solid`);
-    // 2nd div
-    const div_two = document.createElement(`div`);
-    div_two.classList.add(`name`);
-    div_two.innerHTML = `${name} ${download} downloads`
-    // 3rd div
-    const div_three = document.createElement(`div`)
-    div_three.classList.add('options');
-    const create_button = ({ text , func }) =>{
-        const new_button = document.createElement('button');
-        new_button.classList.add('options_btn');
-        new_button.innerHTML = `${text}`;
-        new_button.addEventListener('click' , () => func());
-        return new_button;
-    }
-    // for download return type is a buffer file
-    const btn1 = create_button({
-        text: `Download`, func:() =>  download_file(short_id),
-    });
-    const btn2 = create_button({
-        text: `${locked ? "Hide" :"Unhide"}`, func:() => network_service({
-            path:`/files/${locked ? "unlock" : "lock"}/${short_id}`,
-            // can also be unlock depending on lock status
-            method:`PATCH`,
-            body: undefined,
-        })
-    });
-    const btn3 = create_button({
-        text: `Delete`, func:() =>  network_service({
-            path:`/files/${short_id}`,
-            method:`DELETE`,
-            body: undefined,
-        })
-    });
-    div_three.appendChild(btn1);
-    div_three.appendChild(btn2);
-    div_three.appendChild(btn3);
-    // append to parent
-    li.appendChild(div_one);
-    li.appendChild(div_two);
-    li.appendChild(div_three);
-    files_body?.appendChild(li);
+const li_creator = ({ name, short_id, download, locked }) => {
+	// returns a li --- appended by the looper
+	// create parent
+	const li = document.createElement(`li`);
+	// 1st div
+	const div_one = document.createElement(`div`);
+	div_one.classList.add(`type`);
+	const i = document.createElement(`i`);
+	i.classList.add(`fa-solid`);
+	// 2nd div
+	const div_two = document.createElement(`div`);
+	div_two.classList.add(`name`);
+	div_two.innerHTML = `${name} ${download} downloads`
+	// 3rd div
+	const div_three = document.createElement(`div`)
+	div_three.classList.add('options');
+	const create_button = ({ text, func }) => {
+		const new_button = document.createElement('button');
+		new_button.classList.add('options_btn');
+		new_button.innerHTML = `${text}`;
+		new_button.addEventListener('click', () => func());
+		return new_button;
+	}
+	// for download return type is a buffer file
+	const btn1 = create_button({
+		text: `Download`, func: () => download_file(short_id),
+	});
+	const btn2 = create_button({
+		text: `${locked ? "Hide" : "Unhide"}`, func: () => network_service({
+			path: `/files/${locked ? "unlock" : "lock"}/${short_id}`,
+			// can also be unlock depending on lock status
+			method: `PATCH`,
+			body: undefined,
+		})
+	});
+	const btn3 = create_button({
+		text: `Delete`, func: () => network_service({
+			path: `/files/${short_id}`,
+			method: `DELETE`,
+			body: undefined,
+		})
+	});
+	div_three.appendChild(btn1);
+	div_three.appendChild(btn2);
+	div_three.appendChild(btn3);
+	// append to parent
+	li.appendChild(div_one);
+	li.appendChild(div_two);
+	li.appendChild(div_three);
+	files_body?.appendChild(li);
 }
+
+
 
 
 get_files();
--- src/routes/files.ts
+++ src/routes/files.ts
@@ -26,26 +26,26 @@ const name_gen = () => {
 }
 const upload_dir = `${__dirname}/../uploads`;
 const storage = multer.diskStorage({
-    destination:( req , file , cb) =>{
-        if(!fs.existsSync(upload_dir)){
+    destination: (req, file, cb) => {
+        if (!fs.existsSync(upload_dir)) {
             fs.mkdirSync(upload_dir);
         }
-        cb( null , upload_dir);
+        cb(null, upload_dir);
     },
-    filename: ( req , file , cb) =>{
-        cb(null , `${name_gen()}${path.extname(file.originalname)}`);
+    filename: (req, file, cb) => {
+        cb(null, `${name_gen()}${path.extname(file.originalname)}`);
     },
 });
 
 
-const upload = multer({ 
+const upload = multer({
     storage,
-    limits:{
+    limits: {
         fileSize: 1024 * 1024 * 100 // limit size of 100mb
     },
-    fileFilter:( req , file , cb) =>{
+    fileFilter: (req, file, cb) => {
         // for now allow all files
-        cb( null , true);
+        cb(null, true);
     },
 });
 
@@ -54,67 +54,67 @@ const upload = multer({
 export const file_router = Router();
 
 // download file
-file_router.get('/' , [jwt_key , check_auth] , eah (async ( req:any , res:any) =>{
-    if(!req.user){
+file_router.get('/', [jwt_key, check_auth], eah(async (req: any, res: any) => {
+    if (!req.user) {
         res
-        .status(404)
-        .json({
-            ...ResponseFunction({
-                status:200,
-                message:`User was not found`,
-            }),
-        });
+            .status(404)
+            .json({
+                ...ResponseFunction({
+                    status: 200,
+                    message: `User was not found`,
+                }),
+            });
         return;
     }
     // 
-    const get_user_files = await file_model.find({ owner : req.user._id}).select('-owner -path -_id');
-    if(get_user_files.length == 0){
+    const get_user_files = await file_model.find({ owner: req.user._id }).select('-owner -path -_id');
+    if (get_user_files.length == 0) {
         res
-        .status(200)
-        .json({
-            ...ResponseFunction({
-                status:200,
-                message:`User files fetched`,
-            }),
-            files:[],
-        });
+            .status(200)
+            .json({
+                ...ResponseFunction({
+                    status: 200,
+                    message: `User files fetched`,
+                }),
+                files: [],
+            });
         return;
     }
     res
         .status(200)
         .json({
             ...ResponseFunction({
-                status:200,
-                message:`User files fetched`,
+                status: 200,
+                message: `User files fetched`,
             }),
-            files:get_user_files,
+            files: get_user_files,
         });
 }));
-file_router.get('/download/:id' , [ jwt_key , check_user] , eah( async ( req:any , res:any) =>{
+file_router.get('/download/:id', [jwt_key, check_user], eah(async (req: any, res: any) => {
     // id is the short id of a file
-    if(!req.params.id){
+    if (!req.params.id) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File id was not passed`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File id was not passed`,
+                }),
+            });
         return;
     }
-    const { id }:any = req.params;
+    const { id }: any = req.params;
     // this is the short id
-    const get_file = await file_model.findOne({ short_id : id.trim() })
+    const get_file = await file_model.findOne({ short_id: id.trim() })
 
-    if(!get_file){
+    if (!get_file) {
         res
-        .status(404)
-        .json({
-            ...ResponseFunction({
-                message:`File was not found`,
-                status:404
-            }),
-        });
+            .status(404)
+            .json({
+                ...ResponseFunction({
+                    message: `File was not found`,
+                    status: 404
+                }),
+            });
         return;
     }
 
@@ -123,61 +123,61 @@ file_router.get('/download/:id' , [ jwt_key , check_user] , eah( async ( req:any
         get_file.download++;
         await get_file.save();
         res.download(get_file.path, get_file.name);
-    } else if(!get_file.locked){
+    } else if (!get_file.locked) {
         get_file.download++;
         await get_file.save();
         res.download(get_file.path, get_file.name);
-    }else{
+    } else {
         res
-        .status(401)
-        .json({
-            ...ResponseFunction({
-                message:`File is locked.Unable to download`,
-                status:404,
-            }),
-        });
+            .status(401)
+            .json({
+                ...ResponseFunction({
+                    message: `File is locked.Unable to download`,
+                    status: 404,
+                }),
+            });
     }
 }));
 
 // post file
 
-file_router.post('/' , [ upload.single('file') , jwt_key , check_user] , eah ( async ( req:any , res:any) =>{
-    if(!req.file) {
+file_router.post('/', [upload.single('file'), jwt_key, check_user], eah(async (req: any, res: any) => {
+    if (!req.file) {
         res
-        .status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:'File was not passed',
-            }),
-        });
+            .status(404)
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: 'File was not passed',
+                }),
+            });
         return;
     }
     const short_id = shortid.generate();
     const save_file = await file_model.create({
-        name:req.file.originalname,
-        path:req.file.path,
-        owner:req.user ? new mongoose.Types.ObjectId(req.user._id) : undefined,
+        name: req.file.originalname,
+        path: req.file.path,
+        owner: req.user ? new mongoose.Types.ObjectId(req.user._id) : undefined,
         short_id,
-        locked: req.user ? true : false ,
+        locked: req.user ? true : false,
     });
-    if(!save_file){
+    if (!save_file) {
         res.status(500)
-        .json({
-            ...ResponseFunction({
-                status:500,
-                message:`Unable to save file.Try again later`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 500,
+                    message: `Unable to save file.Try again later`,
+                }),
+            });
         return;
     }
     res.status(200)
-    .json({
-        ...ResponseFunction({
-            message:`File has been saved with an id of${short_id}`,
-            status:200,
-        }),
-    });
+        .json({
+            ...ResponseFunction({
+                message: `File has been saved with an id of${short_id}`,
+                status: 200,
+            }),
+        });
 }));
 
 
@@ -185,219 +185,219 @@ file_router.post('/' , [ upload.single('file') , jwt_key , check_user] , eah ( a
 // unlock file 
 // make private file public
 
-file_router.patch('/unlock/:id' , [ jwt_key , check_user] , eah (async ( req:any , res) =>{
-    if(!req.params.id){
+file_router.patch('/unlock/:id', [jwt_key, check_user], eah(async (req: any, res) => {
+    if (!req.params.id) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File id was not passed`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File id was not passed`,
+                }),
+            });
         return;
     }
     const { id } = req.params;
-    if(!req.user){
+    if (!req.user) {
         res
-        .status(401)
-        .json({
-            ...ResponseFunction({
-                message:`You are not logged in thus cannot unlock this file`,
-                status:401,
-            }),
-        });
+            .status(401)
+            .json({
+                ...ResponseFunction({
+                    message: `You are not logged in thus cannot unlock this file`,
+                    status: 401,
+                }),
+            });
         return;
     }
     // get file and see if it is public
-    const get_file = await file_model.findOne({ short_id : id });
-    if(!get_file){
+    const get_file = await file_model.findOne({ short_id: id });
+    if (!get_file) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File was not found`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File was not found`,
+                }),
+            });
         return;
     }
 
-    if(!get_file.locked){
+    if (!get_file.locked) {
         res.status(200)
-        .json({
-            ...ResponseFunction({
-                status:200,
-                message:`This file is already public`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 200,
+                    message: `This file is already public`,
+                }),
+            });
         return;
     }
 
-    if(!(get_file.owner?.toString() == req.user._id)){
+    if (!(get_file.owner?.toString() == req.user._id)) {
         res.status(401)
-        .json({
-            ...ResponseFunction({
-                status:401,
-                message:`You are not permitted to unlock this file`,
-            }),
-        });
-    }else{
+            .json({
+                ...ResponseFunction({
+                    status: 401,
+                    message: `You are not permitted to unlock this file`,
+                }),
+            });
+    } else {
         get_file.locked = false,
-        await get_file.save();
+            await get_file.save();
         res.status(200)
-        .json({
-            ...ResponseFunction({
-                message:`File has been made public`,
-                status:200,
+            .json({
+                ...ResponseFunction({
+                    message: `File has been made public`,
+                    status: 200,
+                })
             })
-        })
     }
 }));
 
 
 // making files private
-file_router.patch('/lock/:id' , [ jwt_key , check_user] , eah (async ( req:any , res) =>{
-    if(!req.params.id){
+file_router.patch('/lock/:id', [jwt_key, check_user], eah(async (req: any, res) => {
+    if (!req.params.id) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File id was not passed`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File id was not passed`,
+                }),
+            });
         return;
     }
     const { id } = req.params;
-    if(!req.user){
+    if (!req.user) {
         res
-        .status(401)
-        .json({
-            ...ResponseFunction({
-                message:`You are not logged in thus cannot unlock this file`,
-                status:401,
-            }),
-        });
+            .status(401)
+            .json({
+                ...ResponseFunction({
+                    message: `You are not logged in thus cannot unlock this file`,
+                    status: 401,
+                }),
+            });
         return;
     }
     // get file and see if it is public
-    const get_file = await file_model.findOne({ short_id : id });
-    if(!get_file){
+    const get_file = await file_model.findOne({ short_id: id });
+    if (!get_file) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File was not found`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File was not found`,
+                }),
+            });
         return;
     }
 
-    if(get_file.locked){
+    if (get_file.locked) {
         res.status(200)
-        .json({
-            ...ResponseFunction({
-                status:200,
-                message:`This file is already locked`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 200,
+                    message: `This file is already locked`,
+                }),
+            });
         return;
     }
 
-    if(!(get_file.owner?.toString() == req.user._id)){
+    if (!(get_file.owner?.toString() == req.user._id)) {
         res.status(401)
-        .json({
-            ...ResponseFunction({
-                status:401,
-                message:`You are not permitted to lock this file`,
-            }),
-        });
-    }else{
+            .json({
+                ...ResponseFunction({
+                    status: 401,
+                    message: `You are not permitted to lock this file`,
+                }),
+            });
+    } else {
         get_file.locked = true,
-        await get_file.save();
+            await get_file.save();
         res.status(200)
-        .json({
-            ...ResponseFunction({
-                message:`File has been made private`,
-                status:200,
+            .json({
+                ...ResponseFunction({
+                    message: `File has been made private`,
+                    status: 200,
+                })
             })
-        })
     }
 }));
 
 // delete file
-file_router.delete('/:id' , [ jwt_key , check_user ] , eah ( async ( req:any , res) =>{
-    if(!req.params.id){
+file_router.delete('/:id', [jwt_key, check_user], eah(async (req: any, res) => {
+    if (!req.params.id) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                message:`File id was not passed`,
-                status:404,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    message: `File id was not passed`,
+                    status: 404,
+                }),
+            });
         return;
     }
 
     const { id } = req.params;
 
-    const get_file = await file_model.findOne({ short_id : id.trim()});
-    if(!get_file){
+    const get_file = await file_model.findOne({ short_id: id.trim() });
+    if (!get_file) {
         res.status(404)
-        .json({
-            ...ResponseFunction({
-                status:404,
-                message:`File was not found`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 404,
+                    message: `File was not found`,
+                }),
+            });
         return;
     }
-    if(!((get_file.owner && req.user) && (get_file.owner.toString() == req.user._id))){
+    if (!((get_file.owner && req.user) && (get_file.owner.toString() == req.user._id))) {
         // user cannot delete this file
         res.status(401)
-        .json({
-            ...ResponseFunction({
-                status:401,
-                message:`You cannot delete this file as you are not authorized or it is a public file.\nIf you wish to have it deleted and you uploaded the file as a public file kindly contact project manager`,
-            }),
-        });
+            .json({
+                ...ResponseFunction({
+                    status: 401,
+                    message: `You cannot delete this file as you are not authorized or it is a public file.\nIf you wish to have it deleted and you uploaded the file as a public file kindly contact project manager`,
+                }),
+            });
         return;
     }
     // delete file from file system
-    try{
-        if(fs.existsSync(get_file.path)){
+    try {
+        if (fs.existsSync(get_file.path)) {
             fs.unlinkSync(get_file.path);
             const deleted_file = await file_model.findByIdAndDelete(get_file._id);
-            if(!deleted_file){
+            if (!deleted_file) {
                 res.status(500)
-                .json({
-                    ...ResponseFunction({
-                        status:500,
-                        message:`File has been deleted but not fully.`
-                    }),
-                });
+                    .json({
+                        ...ResponseFunction({
+                            status: 500,
+                            message: `File has been deleted but not fully.`
+                        }),
+                    });
                 return;
             }
             res.status(200)
-            .json({
-                ...ResponseFunction({
-                    status:200,
-                    message:`File has been deleted successfully`,
+                .json({
+                    ...ResponseFunction({
+                        status: 200,
+                        message: `File has been deleted successfully`,
+                    })
                 })
-            })
-        }else{
+        } else {
             res.status(404)
+                .json({
+                    ...ResponseFunction({
+                        status: 404,
+                        message: `File not found.Has file been deleted?`,
+                    })
+                })
+        }
+    } catch (error) {
+        res.status(500)
             .json({
                 ...ResponseFunction({
-                    status:404,
-                    message:`File not found.Has file been deleted?`,
+                    message: `Unable to delete file.Try again later`,
+                    status: 500,
                 })
             })
-        }
-    } catch(error){
-        res.status(500)
-        .json({
-            ...ResponseFunction({
-                message:`Unable to delete file.Try again later`,
-                status:500,
-            })
-        })
     }
 }));
